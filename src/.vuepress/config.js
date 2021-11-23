4647df731eac81861d954910e45d53a2d0d21112
Edwin Torres <edwintorres@Edwins-MacBook-Pro.local>
2021-11-23 18:14:45 -0500

    first commit

---
+++ src/.vuepress/config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  dest: 'docs',
+  base: '/doc-restaurante/',
+  title: 'Menú Restaurante',
+  description: 'Documentación Proyecto Menú Restaurante',
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+  themeConfig: {
+    editLinks: false,
+    editLinkText: '',
+    lastUpdated: false,
+    nav: [
+      {
+        text: 'Inicio',
+        link: '/',
+      },
+      {
+        text: 'Repo Doc',
+        link: 'https://github.com/edwintorres1605/doc-restaurante'
+      },
+      {
+        text: 'Repo Front',
+        link: 'https://github.com/edwintorres1605/back-restaurante'
+      },
+      {
+        text: 'Repo Back',
+        link: 'https://github.com/edwintorres1605/front-restaurante'
+      }
+    ],
+    sidebar: [
+      '/'
+    ]
+  },
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-medium-zoom',
+  ]
+}
